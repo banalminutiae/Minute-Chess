b9d0d1b18e4c0ca9afff91ba7b053c881930c736
Andrew Li <andrewli-1920@hotmail.com>
2022-02-22 21:21:02 +0800

    uci to code representaiton implemented

---
+++ Board.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Chess
+{
+	public enum Piece
+	{
+		None,
+		WhitePawn,
+		WhiteKnight,
+		WhiteBishop,
+		WhiteRook,
+		WhiteQueen,
+		WhiteKing,
+		BlackPawn,
+		BlackKnight,
+		BlackBishop,
+		BlackRook,
+		BlackQueen,
+		BlackKing,
+	}
+
+	public struct Move
+	{
+		public byte SrcIdx;
+		public byte DestIdx;
+		public Piece Promotion;
+
+		public Move(byte SrcIdx, byte DestIdx, Piece Promotion)
+		{
+			this.SrcIdx = SrcIdx;
+			this.DestIdx = DestIdx;
+			this.Promotion = Promotion;
+		}
+
+		public Move(string uciMoveNotation)
+		{
+			string fromSquare = uciMoveNotation.Substring(0, 2);
+			string toSquare = uciMoveNotation.Substring(2, 2);
+
+			this.SrcIdx = Notation.ToSquareIndex(fromSquare);
+			this.DestIdx = Notation.ToSquareIndex(toSquare);
+
+			// e7e8q
+			Promotion = (uciMoveNotation.Length == 5) ? Notation.AsciiToPiece(uciMoveNotation[4]) : Piece.None;
+		}
+	}
+
+	public class Board
+	{
+		Piece[] _state = new Piece[64];
+
+		public Piece this[int idx]
+		{
+			get => _state[idx];
+			set => _state[idx] = value;
+		}
+
+		public Piece this[int rank, int file]
+		{
+			get =>_state[rank * 8 + file];
+			set =>_state[rank * 8 + file] = value;
+		}
+
+		public void Play(Move move)
+		{
+			Piece movingPiece = _state[move.SrcIdx];
+			if (move.Promotion != Piece.None)
+			{
+				movingPiece = move.Promotion;
+			}
+
+			_state[move.DestIdx] = movingPiece;
+			_state[move.SrcIdx] = Piece.None;
+		}
+	}
+}
---
+++ Notation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Chess
+{
+	// helper functions for going between uci notation to code representation
+	static class Notation
+	{
+		public static Piece AsciiToPiece(char ascii)
+		{
+			switch (ascii)
+			{
+				case 'P':
+					return Piece.WhitePawn;
+				case 'N':
+					return Piece.WhiteKnight;
+				case 'B':
+					return Piece.WhiteBishop;
+				case 'R':
+					return Piece.WhiteRook;
+				case 'Q':
+					return Piece.WhiteQueen;
+				case 'K':
+					return Piece.WhiteKing;
+				case 'p':
+					return Piece.BlackPawn;
+				case 'n':
+					return Piece.BlackKnight;
+				case 'b':
+					return Piece.BlackBishop;
+				case 'r':
+					return Piece.BlackRook;
+				case 'q':
+					return Piece.BlackQueen;
+				case 'k':
+					return Piece.BlackKing;
+				default:
+					throw new ArgumentException($"Piece character {ascii} not supported.");
+			}
+		}
+
+		public static byte ToSquareIndex(string squareNotation)
+		{
+			int file = squareNotation[0] - 97;
+			int rank = squareNotation[0] - 49;
+
+			int index = rank * 8 + file;
+			if (index >= 0 && index <= 63)
+			{
+				return (byte)index;
+			}
+			else
+			{
+				throw new ArgumentException($"{squareNotation} does not map to valid index between 0 and 63 ");
+			}
+		}
+	}
+}

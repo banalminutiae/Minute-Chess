a3e6c99f21557c10dfa70dc644d81ff68fc0dac0
Andrew Li <andrewli-1920@hotmail.com>
2022-02-22 19:14:41 +0800

    initial commit, program speaks to GUI client through UCI implementation

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Chess
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while(true)
+			{
+				string command = Console.ReadLine().Split()[0];
+				switch (command)
+				{
+					case "uci":
+					{
+						Console.WriteLine("uciok");
+						break;
+					}
+					case "isready":
+					{
+						Console.WriteLine("readyok");
+						break;
+					}
+					case "go":
+					{
+
+						break;
+					}
+					default:
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
+}
